1a5f2f77eaa7b2c9e75421f80eaaa494f6df5466
OmarAl-Khateeb <oalkhateeb2001@gmail.com>
2023-03-05 21:14:39 +0300

    SAD aha x4

--- API/Dtos/ExcerciseSetDto.cs
+++ API/Dtos/ExcerciseSetDto.cs
@@ -8,14 +8,14 @@ namespace API.Dtos
     public class ExcerciseSetDto
     {
         public int Index { get; set; }
-        public int Reps { get; set; }
+        public string Reps { get; set; }
         public bool IsDone { get; set; }
         public int Duration { get; set; }
         public int ExcerciseId { get; set; }
     }
     public class ExcerciseCSetDto
     {
-        public int Reps { get; set; }
+        public string Reps { get; set; }
         public int Duration { get; set; }
         public int ExcerciseId { get; set; }
     }
--- Infrastructure/Data/Course/CourseContextSeed.cs
+++ Infrastructure/Data/Course/CourseContextSeed.cs
@@ -36,20 +36,79 @@ namespace Infrastructure.Data.Course
                     Details = "you dont know what push ups are?",
                     ImageUrl = "https://static.vecteezy.com/system/resources/previews/000/162/096/non_2x/man-doing-push-up-vector-illustration.jpg"
                 };
-                var excerciseSet = new ExcerciseSet
+                var excerciseSet1 = new ExcerciseSet
                 {
+                    Index = 1,
+                    Reps = "10, 10, 12, 12",
+                    ExcerciseId = 1,
+                };
+                var excerciseSet2 = new ExcerciseSet
+                {
+                    Index = 2,
+                    Reps = "10, 10, 12, 12",
+                    ExcerciseId = 1,
+                };
+                var excerciseSet3 = new ExcerciseSet
+                {
+                    Index = 3,
+                    Reps = "10, 10, 12, 12",
+                    ExcerciseId = 1,
+                };
+                var excerciseSet4 = new ExcerciseSet
+                {
+                    Index = 4,
+                    Reps = "10, 10, 12, 12",
+                    ExcerciseId = 1,
+                };
+                var excerciseSet5 = new ExcerciseSet
+                {
+                    Index = 1,
+                    Reps = "10, 10, 12, 12",
+                    ExcerciseId = 1,
+                };
+                var excerciseSet6 = new ExcerciseSet
+                {
+                    Index = 2,
+                    Reps = "10, 10, 12, 12",
+                    ExcerciseId = 1,
+                };
+                var excerciseSet7 = new ExcerciseSet
+                {
+                    Index = 3,
+                    Reps = "10, 10, 12, 12",
+                    ExcerciseId = 1,
+                };
+                var excerciseSet8 = new ExcerciseSet
+                {
+                    Index = 4,
                     Reps = "10, 10, 12, 12",
                     ExcerciseId = 1,
                 };
                 var excercisePlan = new ExcercisePlan
                 {
                     Day = 1,
-                    AppUserId = "1",
+                    AppUserId = "e36c3064-b1e0-493b-a9b5-dcba59c160fc",
                     ExcerciseList = new List<ExcerciseSet>()
                 };
                 course.Excercises.Add(excercisea);
-                excercisePlan.ExcerciseList.Add(excerciseSet);
+                excercisePlan.ExcerciseList.Add(excerciseSet1);
+                excercisePlan.ExcerciseList.Add(excerciseSet2);
+                excercisePlan.ExcerciseList.Add(excerciseSet3);
+                excercisePlan.ExcerciseList.Add(excerciseSet4);
                 course.ExcercisePlans.Add(excercisePlan);
+                
+                await course.SaveChangesAsync();
+                var excercisePlan2 = new ExcercisePlan
+                {
+                    Day = 2,
+                    AppUserId = "e36c3064-b1e0-493b-a9b5-dcba59c160fc",
+                    ExcerciseList = new List<ExcerciseSet>()
+                };
+                excercisePlan2.ExcerciseList.Add(excerciseSet5);
+                excercisePlan2.ExcerciseList.Add(excerciseSet6);
+                excercisePlan2.ExcerciseList.Add(excerciseSet7);
+                excercisePlan2.ExcerciseList.Add(excerciseSet8);
+                course.ExcercisePlans.Add(excercisePlan2);
             }
         await course.SaveChangesAsync();
         }
