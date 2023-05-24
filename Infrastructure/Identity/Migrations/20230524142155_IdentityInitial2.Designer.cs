400fd509c2af131f84985b65350ab33d281af298
OmarAl-Khateeb <oalkhateeb2001@gmail.com>
2023-05-24 17:22:39 +0300

    test3

--- Infrastructure/Identity/Migrations/20230524142155_IdentityInitial2.Designer.cs
+++ Infrastructure/Identity/Migrations/20230524142155_IdentityInitial2.Designer.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using Infrastructure.Data.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -12,9 +13,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Infrastructure.Identity.Migrations
 {
     [DbContext(typeof(AppIdentityDbContext))]
-    partial class AppIdentityDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230524142155_IdentityInitial2")]
+    partial class IdentityInitial2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Infrastructure/Identity/Migrations/20230524142155_IdentityInitial2.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Identity.Migrations
+{
+    /// <inheritdoc />
+    public partial class IdentityInitial2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Index",
+                table: "TransactionTypes");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Index",
+                table: "Transactions",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Index",
+                table: "Transactions");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Index",
+                table: "TransactionTypes",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
